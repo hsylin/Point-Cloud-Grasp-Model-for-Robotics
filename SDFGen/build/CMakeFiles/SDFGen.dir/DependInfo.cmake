a7d885e1933884e983a5a29b8166bc998bd2d10f
Lin <hylin.cs@gmail.com>
2024-09-21 10:18:23 +0800

    message

---
+++ SDFGen/build/CMakeFiles/SDFGen.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/robotic/code/SDFGen/main.cpp" "/home/robotic/code/SDFGen/build/CMakeFiles/SDFGen.dir/main.o"
+  "/home/robotic/code/SDFGen/makelevelset3.cpp" "/home/robotic/code/SDFGen/build/CMakeFiles/SDFGen.dir/makelevelset3.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ SDFGen/build/CMakeFiles/SDFGen.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../bin/SDFGen"
+  "../bin/SDFGen.pdb"
+  "CMakeFiles/SDFGen.dir/main.o"
+  "CMakeFiles/SDFGen.dir/makelevelset3.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SDFGen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
